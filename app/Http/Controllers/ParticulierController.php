3d163ea950bfb13185757e15ecfa252bef7f89d1
SEYDOU DIALLO <dousey2003@gmail.com>
2025-10-01 12:21:33 +0000

    feat: add core models and controllers for document management system
    Add models, controllers, policies, factories, migrations and seeders for:
    - Demande (Request)
    - Document
    - Entreprise (Company)
    - Institution
    - Particulier (Individual)
    - Service

---
+++ app/Http/Controllers/ParticulierController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreParticulierRequest;
+use App\Http\Requests\UpdateParticulierRequest;
+use App\Models\Particulier;
+
+class ParticulierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreParticulierRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Particulier $particulier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Particulier $particulier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateParticulierRequest $request, Particulier $particulier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Particulier $particulier)
+    {
+        //
+    }
+}
